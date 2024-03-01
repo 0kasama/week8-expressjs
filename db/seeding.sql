26e82039706798a7280cbf45e40f4897bbf8b43f
0kasama <okaajyad@gmail.com>
2024-03-01 13:58:40 +0700

    Adding 5 New Data to Actor Table

---
+++ db/seeding.sql
@@ -0,0 +1,6 @@
+insert into actor (actor_id, first_name, last_name) values
+(201, 'Oka', 'Ajyad')
+(202, 'Abdullah', 'Hafidzy')
+(203, 'Radifan', 'Ikramullah')
+(204, 'Son', 'Chaeyoung')
+(205, 'Kim', 'Dami')
