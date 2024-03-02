5a8eb6260830f26802477f62fb12c13e57e43681
0kasama <okaajyad@gmail.com>
2024-03-02 11:36:36 +0700

    Seeding Table

--- db/seeding.sql
+++ db/seeding.sql
@@ -1,6 +1,8 @@
-insert into actor (actor_id, first_name, last_name) values
-(201, 'Oka', 'Ajyad')
-(202, 'Abdullah', 'Hafidzy')
-(203, 'Radifan', 'Ikramullah')
-(204, 'Son', 'Chaeyoung')
-(205, 'Kim', 'Dami')
+INSERT INTO
+    actor (first_name, last_name)
+VALUES
+    ('Oka', 'Ajyad'),
+    ('Abdullah', 'Hafidzy'),
+    ('Radifan', 'Ikramullah'),
+    ('Son', 'Chaeyoung'),
+    ('Kim', 'Dami');
