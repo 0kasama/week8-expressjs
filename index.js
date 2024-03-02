de80958aaf09582743bbc0582ef27c453141a930
0kasama <okaajyad@gmail.com>
2024-03-02 11:38:19 +0700

    Add Query

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const pool = require('./queries');
+const pool = require("./queries");
 
-app.get('/film', (req, res) => {
-    pool.queries('select * from film', (err, result) => {
-        if(err){
-            throw err;
-        }
-        res.send(result.rows);
-    })
+app.get("/film", (req, res) => {
+  pool.query("SELECT * FROM film", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result.rows);
+  });
 });
 
-app.get('/film/id', (req, res) => {
-    pool.queries('select film_id, title from film order by film_id asc', (err, result) => {
-        if(err){
-            throw err;
-        }
-        res.send(result.rows);
-    })
+app.get("/film/id", (req, res) => {
+  pool.query(
+    "SELECT film_id, title FROM film order by film_id asc",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result.rows);
+    }
+  );
 });
 
-app.get('/film/category', (req, res) => {
-    pool.queries('select * from category', (err, result) => {
-        if(err){
-            throw err;
-        }
-        res.send(result.rows);
-    })
+app.get("/film/category", (req, res) => {
+  pool.query("SELECT * FROM category", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result.rows);
+  });
 });
 
-app.get('/film/category', (req, res) => {
-    pool.queries('select film.film_id, film.title as film_film_title, category.category_id, category.name as category_name from film join film_category on film.film_id = film_category.film_id join category on film_category.category_id = category.category_id', (err, result) => {
-        if(err){
-            throw err;
-        }
-        res.send(result.rows);
-    })
+app.get("/category", (req, res) => {
+  pool.query(
+    "SELECT film_id, title, name FROM film INNER JOIN category ON film.film_id = category.category_id",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result.rows);
+    }
+  );
 });
 
+app.get("/actor", (req, res) => {
+  pool.query("SELECT * FROM actor", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result.rows);
+  });
+});
 
-app.listen(3000)
+app.listen(3000);
